93fd610c3edbcc9033fd28ffe0e38f7d5b152b4b
Eric Nishio <ericnishio@protonmail.com>
2016-02-25 09:03:45 +0200

    Rename debug commands

--- src/sprites/traits/can-debug.js
+++ src/sprites/traits/can-debug.js
@@ -2,7 +2,7 @@ import {getTileCoordinate} from 'core/game';
 import Blaster from 'sprites/entities/items/blaster';
 
 export default sprite => {
-  window.idkfa = () => {
+  window.IDKFA = () => {
     const blaster = Blaster(sprite.x, sprite.y);
     sprite.addToInventory(blaster);
     blaster.kill();
@@ -12,7 +12,7 @@ export default sprite => {
    * @param {number} tileX
    * @param {number} tileY
    */
-  window.goto = (tileX, tileY) => {
+  window.GOTO = (tileX, tileY) => {
     sprite.x = getTileCoordinate(tileX);
     sprite.y = getTileCoordinate(tileY);
   };
